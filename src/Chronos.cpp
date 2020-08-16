2a7951549687f2bb98ccc8819ce411939aa1036a
Marcin Sawicki <marcin.z.sawicki@protonmail.com>
2020-08-16 13:36:54 +0200

    Refactor
    Move IO header content to main binary
    It's not used elsewhere

--- src/Chronos.cpp
+++ src/Chronos.cpp
@@ -1,7 +1,7 @@
+#include "fmt/color.h"
 #include "chronos/Coordinator.hpp"
 #include "chronos/Dispatcher.hpp"
 #include "chronos/FileReader.hpp"
-#include "chronos/IO.hpp"
 #include "chronos/Logging.hpp"
 #include "chronos/Parser.hpp"
 #include "chronos/Schedule.hpp"
@@ -21,24 +21,50 @@ namespace chronos
     using task_buidler_t = TaskBuilder<second_clock_t>;
     using parser_t = Parser<task_buidler_t>;
     using file_reader_t = FileReader<parser_t, schedule_t>;
+}
 
-    constexpr int STATUS_OK { 0 };
-    constexpr int STATUS_ERROR { 1 };
+namespace chronos::status
+{
+    constexpr int OK { 0 };
+    constexpr int ERROR { 1 };
+}
 
+namespace chronos::error
+{
+    class WrongNumberOfArguments : public std::runtime_error
+    {
+    public:
+        explicit WrongNumberOfArguments(int args_count)
+                : std::runtime_error(fmt::format(
+                "Invalid number of arguments: {}", args_count)) { }
+    };
+}
+
+namespace chronos::detail
+{
     schedule_ptr_t parse_schedule(const path_t &file_path)
     {
         file_reader_t fileReader;
         return fileReader.read(file_path);
     }
 
-    void show_arg_count_error(const io::error::WrongNumberOfArguments &error)
+    void print_error_message(const std::string &message)
     {
-        chronos::print_error_message(error.what());
+        constexpr auto FOREGROUND_COLOR { fmt::color::crimson };
+        constexpr auto TEXT_EMPHASIS { fmt::emphasis::bold };
+        const auto error_message_formatting {
+                fg(FOREGROUND_COLOR) | TEXT_EMPHASIS };
+        fmt::print(error_message_formatting, message);
+    }
+
+    void show_arg_count_error(const error::WrongNumberOfArguments &error)
+    {
+        print_error_message(error.what());
     }
 
     void show_syntax_error(const parser::error::SyntaxError &error)
     {
-        chronos::print_error_message(fmt::format(
+        print_error_message(fmt::format(
                 "Syntax error: {}", error.what()));
     }
 
@@ -47,33 +73,58 @@ namespace chronos
         dispatcher_t dispatcher(schedule);
         return coordinator_t(dispatcher);
     }
+
+    void validate_arguments_count(int argc)
+    {
+        constexpr auto CORRECT_ARGC { 2 };
+        if (argc != CORRECT_ARGC)
+            throw error::WrongNumberOfArguments(--argc);
+    }
+
+    std::filesystem::path read_file_path_from_arg(int argc, char **argv)
+    {
+        validate_arguments_count(argc);
+        constexpr auto SOURCE_FILE_ARG { 1 };
+        const auto source_path { argv[SOURCE_FILE_ARG] };
+        return std::filesystem::path(source_path);
+    }
+}
+
+namespace chronos
+{
+    int run(int argc, char **argv)
+    {
+        // Setup logger
+        setup_file_logger();
+
+        // Get tasks file path
+        path_t tasks_file_path;
+        try {
+            tasks_file_path = detail::read_file_path_from_arg(argc, argv);
+        }
+        catch (const error::WrongNumberOfArguments &error) {
+            detail::show_arg_count_error(error);
+            return status::ERROR;
+        }
+
+        // Load schedule from file
+        schedule_ptr_t schedule;
+        try {
+            schedule = detail::parse_schedule(tasks_file_path);
+        }
+        catch (const parser::error::SyntaxError &error) {
+            detail::show_syntax_error(error);
+            return status::ERROR;
+        }
+
+        // Create coordinator
+        auto coordinator { detail::setup_coordinator(schedule) };
+
+        return status::OK;
+    }
 }
 
 int main(int argc, char **argv)
 {
-    chronos::setup_file_logger();
-    chronos::schedule_ptr_t schedule;
-
-    chronos::path_t chronos_file_path;
-    try {
-        chronos_file_path = chronos::read_file_path_from_arg(argc, argv);
-    }
-    catch (const chronos::io::error::WrongNumberOfArguments &error) {
-        chronos::show_arg_count_error(error);
-        return chronos::STATUS_ERROR;
-    }
-
-    try {
-        schedule = chronos::parse_schedule(chronos_file_path);
-    }
-    catch (const chronos::parser::error::SyntaxError &error) {
-        chronos::show_syntax_error(error);
-        return chronos::STATUS_ERROR;
-    }
-
-    auto coordinator { chronos::setup_coordinator(schedule) };
-    while (true)
-        coordinator.loop();
-
-    return chronos::STATUS_OK;
-};
+    return chronos::run(argc, argv);
+}
